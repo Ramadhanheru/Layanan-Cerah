bf6d6c79e8a0fbc210f7eebe2379ca6a69c65cbf
Ramadhan Heru <real.ramadhanheru@gmail.com>
2021-05-23 01:24:25 +0700

    login

---
+++ application/controllers/Login.php
@@ -0,0 +1,105 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('login');
+	}
+	public function loginn(){
+		//login admin
+			$this->form_validation->set_rules('username','username','trim|required');
+			$this->form_validation->set_rules('password','Password','trim|required');
+			if($this->form_validation->run()==false){
+				$this->index();
+			
+			}else{
+
+				$userr = $this->input->post('username');
+				$password = $this->input->post('password');
+				
+				$user = $this->Model_data->login($userr);
+				//jika data benar
+				if($user['role'] == '1'){
+					if (password_verify($password, $user['password'])) {
+                   
+                   $this->session->set_userdata(array('user'=>$userr,'password'=>$password,'role' => $user['role']));
+					$this->session->set_flashdata('message','<div class ="alert alert-success" roles="alert">Welcome '.$userr.' ! </div>');
+					redirect('Welcome');
+
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                    Salah Password!</div>');
+                   $this->index();
+                }
+					
+				}else if($user['role'] == '2'){
+					if (password_verify($password, $user['password'])) {
+                   
+                   $this->session->set_userdata(array('user'=>$userr,'password'=>$password,'role' => $user['role'],'id_user'=>$user['id_user']));
+					$this->session->set_flashdata('message','<div class ="alert alert-success" roles="alert">Welcome '.$userr.' ! </div>');
+					redirect('Mekanik/');
+
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                    Salah Password!</div>');
+                   $this->index();
+                }
+					
+				}else if($user['role'] == '3'){
+					if (password_verify($password, $user['password'])) {
+                   
+                   $this->session->set_userdata(array('user'=>$userr,'password'=>$password,'role' => $user['role']));
+					$this->session->set_flashdata('message','<div class ="alert alert-success" roles="alert">Welcome '.$userr.' ! </div>');
+					redirect('Welcome/');
+
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                    Salah Password!</div>');
+                   $this->index();
+                }
+					
+				}else {
+					$this->session->set_flashdata('message','<div class ="alert alert-danger" roles="alert"> Akun Anda tidak terdaftar ! </div>');
+					$this->index();
+				}
+				
+
+			}
+	}
+
+	public function e_profile($id){
+		$data['user'] =  $this->db->get_where('user', ['user' => $this->session->userdata('user')])->row_array();
+		$data ['query']= $this->Model_data->ambil_id_user($id);
+
+		$this->form_validation->set_rules('user','user','required|trim');
+        $this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[3]', ['min_length' => 'Kata sandi terlalu pendek!']);
+
+        if($this->form_validation->run()==false){
+		$this->load->view('template/sidebar');
+		$this->load->view('template/topbar' ,$data);
+		$this->load->view('e_profile',$data);
+		$this->load->view('template/footer');
+
+		}else{
+		
+		$this->db->set('user', $this->input->post('user'));
+		$this->db->set('password', password_hash($this->input->post('password'), PASSWORD_DEFAULT));
+		$this->db->where('id_user', $id);
+		$this->db->update('user');
+			$this->session->set_flashdata('message','<div class ="alert alert-success" roles="alert"> Data berhasil diubah ! 
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span></button> </div>');
+			redirect('welcome');
+		}
+	}
+
+
+
+	public function logout(){
+		unset($role);
+		$this->session->sess_destroy();
+		$this->session->set_flashdata('message','<div class ="alert alert-warning" roles="alert"> Log out berhasil </div>');
+		 $this->index();
+	}
+}
---
+++ application/views/login.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Login - srtdash</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="<?= base_url('') ?>assets/images/icon/favicon.ico">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>assets/css/themify-icons.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>assets/css/metisMenu.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>assets/css/slicknav.min.css">
+    <!-- amchart css -->
+    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+    <!-- others css -->
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>assets/css/typography.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>assets/css/default-css.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>assets/css/styles.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>assets/css/responsive.css">
+    <!-- modernizr css -->
+    <script src="<?= base_url('asset/') ?>assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- login area start -->
+    <div class="login-area" style="background-color: #BABBFF;">
+        <div class="container">
+            <div class="login-box ptb--100">
+                <form action="<?= base_url('login/loginn'); ?>" method="post">
+                    <div class="login-form-head" style="background-color: #56369E;">
+                        <h4>layanan cerah </h4><p>by</p>
+                        <div>
+                            <img src="<?= base_url('asset/logo1.png') ?>">
+                        </div>
+                    </div>
+                    <div class="login-form-body">
+                        <?= $this->session->flashdata('message');?>
+                        <div class="form-gp">
+                            <label for="exampleInputEmail1">Username</label>
+                            <input type="text" id="username" name="username">
+                            <?= form_error('username','<small class="text-danger pl-3 ">','</small>');?>
+                            <i class="ti-user"></i>
+                            <div class="text-danger"></div>
+                        </div>
+                        <div class="form-gp">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" id="password" name="password">
+                            <?= form_error('password','<small class="text-danger pl-3 ">','</small>');?>
+                            <i class="ti-lock"></i>
+                            <div class="text-danger"></div>
+                        </div>
+                        <div class="submit-btn-area">
+                            <button id="form_submit" type="submit">LOG IN <i class="ti-arrow-right"></i></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- login area end -->
+
+    <!-- jquery latest version -->
+    <script src="<?= base_url('asset/') ?>assets/js/vendor/jquery-2.2.4.min.js"></script>
+    <!-- bootstrap 4 js -->
+    <script src="<?= base_url('asset/') ?>assets/js/popper.min.js"></script>
+    <script src="<?= base_url('asset/') ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?= base_url('asset/') ?>assets/js/owl.carousel.min.js"></script>
+    <script src="<?= base_url('asset/') ?>assets/js/metisMenu.min.js"></script>
+    <script src="<?= base_url('asset/') ?>assets/js/jquery.slimscroll.min.js"></script>
+    <script src="<?= base_url('asset/') ?>assets/js/jquery.slicknav.min.js"></script>
+    
+    <!-- others plugins -->
+    <script src="<?= base_url('asset/') ?>assets/js/plugins.js"></script>
+    <script src="<?= base_url('asset/') ?>assets/js/scripts.js"></script>
+</body>
+
+</html>
